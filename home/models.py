85366c09732ed9e912a93dcaf948f46acdc8665c
Khairul Gazali <khairulgazali10@gmail.com>
2021-05-24 08:35:19 +0800

    Menambahkan verbose name ke admin site

--- home/models.py
+++ home/models.py
@@ -8,6 +8,7 @@ class ServiceIndeks(models.Model):
 
     class Meta:
         ordering = ['-tahun_pelayanan']
+        verbose_name_plural = "Indeks Pelayanan"
 
     def __str__(self):
         return str(self.tahun_pelayanan)
@@ -30,42 +31,60 @@ class Sample(models.Model):
     kategori = models.CharField(max_length=1, choices=CATEGORY, default='X')
 
     def __str__(self):
-        return str(self.nama)
+        return self.nama
 
 
 class Calibration(models.Model):
     nama = models.CharField(max_length=200, unique=True)
     keterangan = models.TextField(blank=True)
 
+    class Meta:
+        verbose_name_plural = "Kalibrasi"
+
     def __str__(self):
-        return str(self.nama)
+        return self.nama
 
 
 class Certification(models.Model):
     nama = models.CharField(max_length=200, unique=True)
     keterangan = models.TextField(blank=True)
 
-
-class Consultation(models.Model):
-    nama = models.CharField(max_length=200, unique=True )
-    keterangan = models.TextField(blank=True, null=True, help_text='Masukkan keterangan topik konsultasi')
+    class Meta:
+        verbose_name_plural = "Sertifikasi"
 
     def __str__(self):
-        return str(self.nama[:15])
+        return self.nama
+
+
+class Consultation(models.Model):
+    nama = models.CharField(max_length=200, unique=True)
+    keterangan = models.TextField(blank=True, null=True, help_text='Masukkan keterangan topik konsultasi')
+
+    class Meta:
+        verbose_name_plural = "Konsultasi Teknologi"
+
+    def __str__(self):
+        return self.nama[:15]
 
 
 class BussinesIncubator(models.Model):
-    klien = models.CharField(max_length=200, unique=True )
+    klien = models.CharField(max_length=200, unique=True)
     alamat = models.TextField(blank=True, null=True, help_text='Masukkan keterangan sampel')
     keterangan = models.TextField(blank=True, null=True, help_text='Masukkan keterangan sampel')
 
+    class Meta:
+        verbose_name_plural = "Inkubator Bisnis"
+
     def __str__(self):
-        return str(self.klien)
+        return self.klien
 
 
 class Training(models.Model):
     nama = models.CharField(max_length=200, unique=True)
     keterangan = models.TextField(blank=True, null=True, help_text='Masukkan keterangan pelatihan')
 
+    class Meta:
+        verbose_name_plural = "Pelatihan"
+
     def __str__(self):
-        return str(self.nama)
+        return self.nama
